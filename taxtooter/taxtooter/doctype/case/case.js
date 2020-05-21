87b91450bde2ab01aa89bbfa4e7d80392d911171
NischayaSharma <nischaya1703@gmail.com>
2020-05-21 22:38:22 +0530

    Initial commit

---
+++ taxtooter/taxtooter/doctype/case/case.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, me and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Case', {
+	refresh: function(frm) {
+
+	}
+});
---
+++ taxtooter/taxtooter/doctype/sub_query/sub_query.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, me and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('sub_query', {
+	refresh: function(frm) {
+
+	}
+});
